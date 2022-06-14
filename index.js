7888e1e7987c2b228fb35b3b088af87bfd7882a6
brandonblack <brandonblack02@gmail.com>
2022-06-13 18:49:55 -0700

    Basic setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("heelow")
+const express = require('express');
+const app = express();
+const PORT = "8080";
+
+app.listen(PORT, () => {
+    console.log(`Connected to backend on Port: ${PORT}`);
+});
+
+app.get('/home', (req, res) => {
+    res.json({'message': 'Hello world'});
+})
